1e58d2419c131ba32ab1fc9bcb1f2f13acf07cd3
Sonu-kumawat-ai <kumawatsonu086@gmail.com>
2025-08-28 16:11:34 +0530

    Done

---
+++ convert to roman numerals.py
@@ -0,0 +1,19 @@
+def roman(num):
+    if(num>3999):
+        print("Invaild Number")
+    rom=[1000,900,500,400,100,90,50,40,10,9,5,4,1]
+    value=["M","CM","D","CD","C","XC","L","XL","X","IX","V","IV","I"]
+    roman=""
+    i=0    
+    while num>0:
+        div=num//rom[i] 
+        num=num%rom[i] 
+        while div:
+            roman=roman+value[i]
+            div=div-1
+        i=i+1
+    return roman
+
+num=int(input("Enter the Number : "))
+result=roman(num)
+print("The Roman Numeral of ",num,"is : ",result)
